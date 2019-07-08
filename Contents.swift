865060ada41f24ab6d6c59db350beaab1a512bc3
Apple <apple@Apples-MacBook-Pro-7.local>
2019-07-08 15:42:01 -0500

    second commit

---
+++ Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+//this is a comment! Please use comments to help you keep
+    //track of your code!
+
+
+
+
+
+//Intention: Working with integers and doubles
+//Data Types: Ints and Doubles
+
+var int = 4
+var double = 5.0
+
+print(Double(int) * double)
+
+var a = 12.4
+var b = 65.0
+var c = 31.0
+var d = 98.0
+
+print(((a+b+c)+Double(d))/4)
+
+
+
